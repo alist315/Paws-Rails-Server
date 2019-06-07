db2908b8e17f15ce94b85b853629545c6e2febd3
alist315 <aaron.reinheimner315@gmail.com>
2019-06-07 16:44:41 -0400

    First Commit

---
+++ paws_and_found/app/controllers/pets_controller.rb
@@ -0,0 +1,26 @@
+class PetsController < ApplicationController
+  # index route
+  def index
+    render json: Pet.all
+  end
+
+  #show route
+  def show
+    render json: Pet.find(params["id"])
+  end
+
+  # create route
+  def create
+    render json: Pet.create(params["Pet"])
+  end
+
+  # delete route
+  def delete
+    render json: Pet.delete(params["id"])
+  end
+
+  # update route
+  def update
+    render json: Pet.update(params["id"], params["Pet"])
+  end
+end
---
+++ paws_and_found/app/models/pet.rb
@@ -0,0 +1,7 @@
+class Pet
+DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'paws_and_found_development'})
+
+
+
+
+end
