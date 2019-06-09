3cf23e63c8b411be547bb04ec5befeef967f0cfb
alist315 <aaron.reinheimner315@gmail.com>
2019-06-09 17:11:19 -0400

    error fix

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,27 @@
+class PetsController < ApplicationController
+  # index route
+  def index
+    render json: Pet.all
+  end
+
+  #show route
+  def show
+    render json: Pet.find(params["id"])
+  end
+
+  # create route
+  def create
+    p 'create method in controller'
+    render json: Pet.create(params["pet"])
+  end
+
+  # delete route
+  def delete
+    render json: Pet.delete(params["id"])
+  end
+
+  # update route
+  def update
+    render json: Pet.update(params["id"], params["pet"])
+  end
+end
---
+++ app/models/pet.rb
@@ -0,0 +1,81 @@
+class Pet
+DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'paws_and_found_development'})
+
+def self.all
+    results = DB.exec("SELECT * FROM pets;")
+    return results.map do |result|
+        {
+            "id" => result["id"].to_i,
+            "name" => result["name"],
+            "species" => result["species"],
+            "breed" => result["breed"],
+            "photo" => result["photo"],
+            "last_known_location" => result["last_known_location"],
+            "description" => result["description"],
+            "found" => result["found"]
+        }
+    end
+end
+
+def self.find(id)
+    results = DB.exec("SELECT * FROM pets WHERE id=#{id};")
+    return {
+      "id" => results.first["id"].to_i,
+      "name" => results.first["name"],
+      "species" => results.first["species"],
+      "breed" => results.first["breed"],
+      "photo" => results.first["photo"],
+      "last_known_location" => results.first["last_known_location"],
+      "description" => results.first["description"],
+      "found" => results.first["found"]
+    }
+end
+
+def self.create(opts)
+  p 'inside create method'
+  p opts
+    results = DB.exec(
+        <<-SQL
+            INSERT INTO pets (name, species, breed, photo, last_known_location, description, found)
+            VALUES ('#{opts["name"]}', '#{opts["species"]}', '#{opts["breed"]}', '#{opts["photo"]}', '#{opts["last_known_location"]}', '#{opts["description"]}', '#{opts["found"]}')
+            RETURNING id, name, species, breed, photo, last_known_location, description, found;
+        SQL
+    )
+    return {
+      "id" => results.first["id"].to_i,
+      "name" => results.first["name"],
+      "species" => results.first["species"],
+      "breed" => results.first["breed"],
+      "photo" => results.first["photo"],
+      "last_known_location" => results.first["last_known_location"],
+      "description" => results.first["description"],
+      "found" => results.first["found"]
+    }
+end
+
+def self.delete(id)
+    results = DB.exec("DELETE FROM pets WHERE id=#{id};")
+    return { "deleted" => true }
+end
+
+def self.update(id, opts)
+    results = DB.exec(
+        <<-SQL
+            UPDATE pets
+            SET name='#{opts["name"]}', species='#{opts["species"]}', breed='#{opts["breed"]}', photo='#{opts["photo"]}', last_known_location='#{opts["last_known_location"]}', description='#{opts["description"]}', found='#{opts["found"]}'
+            WHERE id=#{id}
+            RETURNING id, name, species, breed, photo, last_known_location, description, found;
+        SQL
+    )
+    return {
+      "id" => results.first["id"].to_i,
+      "name" => results.first["name"],
+      "species" => results.first["species"],
+      "breed" => results.first["breed"],
+      "photo" => results.first["photo"],
+      "last_known_location" => results.first["last_known_location"],
+      "description" => results.first["description"],
+      "found" => results.first["found"]
+    }
+end
+end
