f2b76148da9c9d29980cf9ef7a2ca757c32f4b3d
alist315 <aaron.reinheimner315@gmail.com>
2019-06-09 17:17:12 -0400

    heroku error

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,10 @@
 class Pet
-DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'paws_and_found_development'})
+  if(ENV['DATABASE_URL'])
+          uri = URI.parse(ENV['DATABASE_URL'])
+          DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+      else
+          DB = PG.connect(host: "localhost", port: 5432, dbname: 'simplerails')
+      end
 
 def self.all
     results = DB.exec("SELECT * FROM pets;")
