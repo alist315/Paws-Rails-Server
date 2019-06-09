5b679f4a1f64008d8fc9af14fc7d6ab0bfe313ae
alist315 <aaron.reinheimner315@gmail.com>
2019-06-09 17:30:36 -0400

    Heroku is up!

--- paws_and_found/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-get '/pets', to: 'pets#index'
-get '/pets/:id', to: 'pets#show'
-post '/pets', to: 'pets#create'
-delete '/pets/:id', to: 'pets#delete'
-put '/pets/:id', to: 'pets#update'
-end
